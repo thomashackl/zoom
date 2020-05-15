4f8999d99a184c4452ab502456ae2a6702476143
Thomas Hackl <thomas.hackl@uni-passau.de>
2020-05-15 09:28:46 +0200

    process webinars in cronjob

--- ZoomMeetingDateCronjob.php
+++ ZoomMeetingDateCronjob.php
@@ -106,7 +106,8 @@ class ZoomMeetingDateCronjob extends CronJob {
                         $newSettings->start_time =
                             $startTime->format('Y-m-d') . 'T' . $startTime->format('H:i:s');
                         $newSettings->duration = $duration;
-                        $result = ZoomAPI::updateMeeting($m->zoom_settings->host_id, $m->zoom_meeting_id, $newSettings);
+                        $result = ZoomAPI::updateMeeting($m->zoom_settings->host_id, $m->zoom_meeting_id,
+                            $newSettings, $m->webinar);
 
                         if ($result !== null && $result !== 404) {
                             if ($parameters['verbose']) {
